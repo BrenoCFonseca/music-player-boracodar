ff063cb00b95b026fdf1f762bc7ed4a3f08547b9
brenofoz <106184416+brenofoz@users.noreply.github.com>
2023-05-24 15:58:17 -0300

    Add files via upload

---
+++ src/js/songs.js
@@ -0,0 +1,32 @@
+export default [
+    {
+      src: "./src/music/Dark Tranquility - Anno Domini Beats.mp3",
+      name: "Dark Tranquility",
+      autor: "Anno Domini Beats",
+    },
+    {
+      src: "./src/music/It Starts Here- FiftySounds.mp3",
+      name: "It Starts Here",
+      autor: "FiftySounds",
+    },
+    {
+      src: "./src/music/Like That - Anno Domini Beats.mp3",
+      name: "Like That",
+      autor: "Anno Domini Beats"
+    },
+    {
+      src: "./src/music/Meeting Point - FiftySounds.mp3",
+      name: "Meeting Point",
+      autor: "FiftySounds",
+    },
+    {
+      src: "./src/music/Shake - Anno Domini Beats.mp3",
+      name: "Shake",
+      autor: "Anno Domini Beats",
+    },
+    {
+      src: "./src/music/Spiritual - FiftySounds.mp3",
+      name: "Spiritual",
+      autor: "FiftySounds",
+    },  
+  ];
